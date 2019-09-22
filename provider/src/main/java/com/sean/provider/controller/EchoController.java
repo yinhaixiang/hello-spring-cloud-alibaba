c5931d72fc6bcc5db84c10508041bdddbcaaba5d
殷海翔 <yin.hx@burgeon.cn>
2019-09-22 21:26:51 +0800

    init

---
+++ provider/src/main/java/com/sean/provider/controller/EchoController.java
@@ -0,0 +1,13 @@
+package com.sean.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+    @GetMapping(value = "/echo/{message}")
+    public String echo(@PathVariable String message) {
+        return "Hello Nacos Discovery " + message;
+    }
+}
