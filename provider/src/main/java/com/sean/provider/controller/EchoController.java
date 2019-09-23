101495518e9d23e5981ba44f2449253f3b5128a7
殷海翔 <yin.hx@burgeon.cn>
2019-09-23 09:29:41 +0800

    init

--- provider/src/main/java/com/sean/provider/controller/EchoController.java
+++ provider/src/main/java/com/sean/provider/controller/EchoController.java
@@ -1,13 +1,18 @@
 package com.sean.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
+
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
-        return "Hello Nacos Discovery " + message;
+        return "Hello Nacos Discovery " + message + " i am from " + port;
     }
 }
