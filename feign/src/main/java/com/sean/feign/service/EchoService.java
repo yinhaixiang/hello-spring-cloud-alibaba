004e9734ff4e23a4c8a2c9f91210181a32074c2e
殷海翔 <yin.hx@burgeon.cn>
2019-09-23 15:28:17 +0800

    增加sentinel

--- feign/src/main/java/com/sean/feign/service/EchoService.java
+++ feign/src/main/java/com/sean/feign/service/EchoService.java
@@ -1,10 +1,11 @@
 package com.sean.feign.service;
 
+import com.sean.feign.service.fallback.EchoServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "nacos-provider")
+@FeignClient(value = "nacos-provider", fallback = EchoServiceFallback.class)
 public interface EchoService {
 
     @GetMapping(value = "/echo/{message}")
---
+++ feign/src/main/java/com/sean/feign/service/fallback/EchoServiceFallback.java
@@ -0,0 +1,12 @@
+package com.sean.feign.service.fallback;
+
+import com.sean.feign.service.EchoService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EchoServiceFallback implements EchoService {
+    @Override
+    public String echo(String message) {
+        return "echo fallback";
+    }
+}
