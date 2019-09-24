dca433871e36c96de0abeb71d92fdb26eb945067
殷海翔 <yin.hx@burgeon.cn>
2019-09-24 09:52:07 +0800

    增加spring-cloud-nacos-config

--- provider/src/main/java/com/sean/provider/controller/EchoController.java
+++ provider/src/main/java/com/sean/provider/controller/EchoController.java
@@ -1,6 +1,8 @@
 package com.sean.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,18 @@ public class EchoController {
     @Value("${server.port}")
     private String port;
 
+    // 注入配置文件上下文
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
         return "Hello Nacos Discovery " + message + " i am from " + port;
+    }
+
+    // 从上下文中读取配置
+    @GetMapping(value = "/hi")
+    public String sayHi() {
+        return "Hello " + applicationContext.getEnvironment().getProperty("user.name");
     }
 }
